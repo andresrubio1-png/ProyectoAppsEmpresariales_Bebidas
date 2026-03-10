600795ddbccc0f3e6cf023d40cba5b96e3c6be35
Andres Rubio <andres.rubio1@estudiantesunibague.edu.co>
2026-03-10 15:41:33 -0500

    Creacion del service de gui

---
+++ src/main/java/com/mycompany/proyectoempresariales/Controlador/GuiService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoempresariales.Controlador;
+
+import com.mycompany.proyectoempresariales.Vista.IGuiCambiable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public class GuiService {
+    private static List <IGuiCambiable> guis = new ArrayList<>();
+    
+    public static void registrarGUI(IGuiCambiable gui){
+        guis.add(gui);
+    }
+    
+    public static void eliminarGUI(IGuiCambiable gui){
+        guis.remove(gui);
+    }
+    
+    private static void cambioEnGUI(){
+        for(IGuiCambiable gui : guis){
+            gui.cambio();
+        }
+    }
+}
