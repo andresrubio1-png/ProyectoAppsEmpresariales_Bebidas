ca220ea34e563b41bde060b327724066a773dc3f
Andres Rubio <andres.rubio1@estudiantesunibague.edu.co>
2026-03-10 15:07:59 -0500

    Cambio en formato

--- src/main/java/com/mycompany/proyectoempresariales/Modelo/Proveedor.java
+++ src/main/java/com/mycompany/proyectoempresariales/Modelo/Proveedor.java
@@ -41,7 +41,7 @@ public class Proveedor {
     }
 
     // ===============================
-    // OPERACIONES SOBRE PROVEEDORES
+    // OPERACIONES SOBRE PROVEEDORESS
     // ===============================
 
     // Crear proveedor
