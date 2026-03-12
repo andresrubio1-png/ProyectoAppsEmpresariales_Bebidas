0ef1dee464d81ce2449341329ad94452161ca126
Andres Rubio <andres.rubio1@estudiantesunibague.edu.co>
2026-03-12 18:21:40 -0500

    Cambios jframe Proveedores

--- src/main/java/com/mycompany/proyectoempresariales/Vista/GUIAgregarProveedor.java
+++ src/main/java/com/mycompany/proyectoempresariales/Vista/GUIAgregarProveedor.java
@@ -39,7 +39,7 @@ public class GUIAgregarProveedor extends javax.swing.JFrame {
         txtTelefonoProveedor = new javax.swing.JTextField();
         btnAgregarProveedor = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel1.setText("Agregar Proveedor");
