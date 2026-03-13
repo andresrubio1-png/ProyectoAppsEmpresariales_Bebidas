305a95415ca043e0975b4d3e02ac0ea2575103c1
Andres Rubio <andres.rubio1@estudiantesunibague.edu.co>
2026-03-12 21:25:11 -0500

    Implementacion de singleton

--- src/main/java/com/mycompany/proyectoempresariales/Vista/GUIPrecioVentaLicor.java
+++ src/main/java/com/mycompany/proyectoempresariales/Vista/GUIPrecioVentaLicor.java
@@ -5,6 +5,7 @@
 package com.mycompany.proyectoempresariales.Vista;
 
 import com.mycompany.proyectoempresariales.Controlador.BebidaService;
+import com.mycompany.proyectoempresariales.Controlador.IBebidaService;
 import com.mycompany.proyectoempresariales.Modelo.Alcoholica;
 import javax.swing.JOptionPane;
 
@@ -13,7 +14,7 @@ import javax.swing.JOptionPane;
  * @author andre
  */
 public class GUIPrecioVentaLicor extends javax.swing.JFrame {
-    
+    private IBebidaService bebidaService = BebidaService.getInstance();
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GUIPrecioVentaLicor.class.getName());
 
     /**
@@ -21,6 +22,7 @@ public class GUIPrecioVentaLicor extends javax.swing.JFrame {
      */
     public GUIPrecioVentaLicor() {
         initComponents();
+        
         setLocationRelativeTo(this);
     }
 
@@ -192,7 +194,7 @@ public class GUIPrecioVentaLicor extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         int Codigo = Integer.parseInt(txtCodigo.getText());
-        Alcoholica A = (Alcoholica) BebidaService.BuscarBebida(Codigo);
+        Alcoholica A = (Alcoholica) bebidaService.BuscarBebida(Codigo);
         if(A!= null){
             llenardatos(A);
         }else
@@ -202,27 +204,7 @@ public class GUIPrecioVentaLicor extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new GUIPrecioVentaLicor().setVisible(true));
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
