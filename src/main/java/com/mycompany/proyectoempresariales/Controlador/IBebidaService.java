c8d51baec3912ff168417bc5e744a19940642c62
Andres Rubio <andres.rubio1@estudiantesunibague.edu.co>
2026-03-12 21:20:10 -0500

    Creacion de interfaz de BebidaService

---
+++ src/main/java/com/mycompany/proyectoempresariales/Controlador/IBebidaService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.proyectoempresariales.Controlador;
+
+import com.mycompany.proyectoempresariales.Modelo.Alcoholica;
+import com.mycompany.proyectoempresariales.Modelo.Bebida;
+import com.mycompany.proyectoempresariales.Modelo.Gaseosa;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public interface IBebidaService {
+    public  Bebida addBebidas(Bebida bebida);
+    public Bebida BuscarBebida (int Codigo);
+    public  List<Bebida> listarBebidas();
+    public  List<Gaseosa> listarGaseosas();
+    public  List<Alcoholica> listarLicores();
+    public  void eliminardelSistema(Bebida b);
+}
